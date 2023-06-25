23e1122a6d66e4d541a0a8bc818a45da88cbae15
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-06-25 16:37:49 +0900

    backend app 생성

---
+++ ymcbackend/src/main/java/com/example/ymcbackend/YmcbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ymcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmcbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmcbackendApplication.class, args);
+	}
+
+}
---
+++ ymcbackend/src/test/java/com/example/ymcbackend/YmcbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ymcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmcbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
